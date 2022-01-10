3bb5633171daa7ec616e9bbbe3f4f2cc70cb20ea
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-11 00:50:00 +0500

    wrote about API, HTTP, RestApi & express.js

---
+++ server.js
@@ -0,0 +1,13 @@
+//! basic knowledge on HTTP, API, RESTAPI, & EXPRESS.JS
+/*
+Clinet va Server orasidagi aloqani "http" yaratadi va API "http" yaratgan aloqada xizmatchi bo`ladi
+API ni dasturchila yaratadi 
+restourant bu "http", APIlar bu afitsantlar ya`ni oshpazlar(server) va clientlar orasida xizmat qilib yuradi!
+!RestApi: Rest---> arxitektura va tushuncha(Umumiy keladigan ma`lumotni xolati va formati)
+Asosan "http" yordamida ko`tarilgan "API" lar "RestApi" lar deyiladi yoki http bilan eng yaxshi chiqisha oladigan APIlar "RestApi"lar deyiladi!
+!dependency:
+bu proekt tuzish uchun kerak bo`ladigan texnalogiyalar(packagelar)!
+!dev-dependency:
+dasturchiga local development va testing uchun kerak bo`ladigan texnalogiya(packagelar)!(example: nodemon)
+
+*/
